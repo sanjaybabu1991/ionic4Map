1688b32a4e13ae0bbb643d3f76b9916f76643f4f
unknown <sbabu@iiserb.ac.in>
2018-06-18 22:13:28 +0530

    commit

--- SANJAY/Ionic/auth/auth/www/build/1.js
+++ SANJAY/Ionic/auth/auth/www/build/1.js
@@ -15,6 +15,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 
+//import { IonicModule } from 'ionic-angular';
 
 var ResetPasswordModule = /** @class */ (function () {
     function ResetPasswordModule() {
