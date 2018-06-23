dabeebecdcfcb78d1e7ad2b7e7bc249f756bf334
unknown <sbabu@iiserb.ac.in>
2018-06-23 22:15:31 +0530

    commit

--- SANJAY/Ionic/auth/auth/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ SANJAY/Ionic/auth/auth/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,30 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-appversion.RareloopAppVersion",
-    "file": "plugins/cordova-plugin-appversion/www/app-version.js",
-    "pluginId": "cordova-plugin-appversion",
-    "clobbers": [
-      "AppVersion"
-    ]
-  },
-  {
-    "id": "cordova-plugin-app-update.AppUpdate",
-    "file": "plugins/cordova-plugin-app-update/www/AppUpdate.js",
-    "pluginId": "cordova-plugin-app-update",
-    "clobbers": [
-      "AppUpdate"
-    ]
-  },
-  {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
-  },
-  {
     "id": "cordova-plugin-geolocation.geolocation",
     "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
     "pluginId": "cordova-plugin-geolocation",
@@ -39,27 +15,11 @@ module.exports = [
     "runs": true
   },
   {
-    "id": "cordova-plugin-ionic-keyboard.keyboard",
-    "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
-    "pluginId": "cordova-plugin-ionic-keyboard",
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
     "clobbers": [
-      "window.Keyboard"
-    ]
-  },
-  {
-    "id": "cordova-plugin-sim.Sim",
-    "file": "plugins/cordova-plugin-sim/www/sim.js",
-    "pluginId": "cordova-plugin-sim",
-    "merges": [
-      "window.plugins.sim"
-    ]
-  },
-  {
-    "id": "cordova-plugin-sim.SimAndroid",
-    "file": "plugins/cordova-plugin-sim/www/android/sim.js",
-    "pluginId": "cordova-plugin-sim",
-    "merges": [
-      "window.plugins.sim"
+      "device"
     ]
   },
   {
@@ -69,20 +29,25 @@ module.exports = [
     "clobbers": [
       "navigator.splashscreen"
     ]
+  },
+  {
+    "id": "cordova-plugin-ionic-keyboard.keyboard",
+    "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
+    "pluginId": "cordova-plugin-ionic-keyboard",
+    "clobbers": [
+      "window.Keyboard"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-appversion": "1.0.0",
-  "cordova-plugin-app-update": "1.6.0",
-  "cordova-plugin-device": "2.0.2",
   "cordova-plugin-geolocation": "4.0.1",
-  "cordova-plugin-ionic-keyboard": "2.0.5",
-  "cordova-plugin-ionic-webview": "1.1.19",
-  "cordova-plugin-sim": "1.3.3",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.2",
   "cordova-plugin-splashscreen": "5.0.2",
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-ionic-webview": "1.1.19",
+  "cordova-plugin-ionic-keyboard": "2.0.5"
 };
 // BOTTOM OF METADATA
 });
